97132d3c4ee773623356a9c87a865b883421f70a
Will <72624648+wgozzard@users.noreply.github.com>
2022-02-05 13:35:12 -0500

    Update coffee_shop_CA.py

--- coffee_shop_CA.py
+++ coffee_shop_CA.py
@@ -1,50 +1,61 @@
-# Define your functions
+import sys
+from utils import print_message, get_size, order_latte
+
 def coffee_bot():
-  print('Welcome to the cafe')
-  
-  size = get_size()
-  drink_type = get_drink_type()
-  print("Alright, that's a {} {}!\n".format(size, drink_type))
-  name = input('Can I get your name please? ')
-  print("Thanks, {}! Your drink will be ready shortly".format(name))
-  
+  print('Welcome to the cafe!')
+  drinks = []
+  order_drinks = True
+  while order_drinks:
+    size = get_size()  
+    drink_type = get_drink_type()
 
-def get_size():
-  res = input('What size drink can I get for you? \n [a] Small \n [b] Medium \n [c] Large \n')
-  if res == 'a':
-    return 'Small'
-  elif res == 'b':
-    return 'Medium'
-  elif res == 'c':
-    return 'Large'
-  else:
-    print_message()
-    return get_size()
+    drink = '{} {}'.format(size, drink_type)
+    print('Alright, that\'s a {}!'.format(drink))
+    drinks.append(drink)
+    while True:
+      order_drinks = input('Would you like to order another drink? (y/n)\n>')
+      if order_drinks in ['y', 'yes', 'sure']:
+        order_drinks = True
+        break
+      
+      elif order_drinks in ['n', 'no']:
+        order_drinks = False
+        break
 
-def print_message():
-  print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.\n")
+  print('Okay, so I have:')
+  for drink in drinks:
+    print('-', drink)
+  
+  #name = input('Can I get your name please? \n> ')
+  #print('Thanks, {}! Your order will be ready shortly.'.format(name))
 
 def get_drink_type():
-  res = input('What type of drink would you like? \n [a] Brewed Coffee \n [b] Mocha \n [c] Latte \n')
-  
+  res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
+  stop_words = ['bye', 'stop']
   if res == 'a':
-    return 'Brewed Coffee'
+    return 'brewed coffee'
   elif res == 'b':
-    return 'Mocha'
+    return order_mocha()
   elif res == 'c':
     return order_latte()
+  elif res in stop_words:
+    print("order cancelled")
+    sys.exit()
   else:
     print_message()
     return get_drink_type()
+  
+# Define new functions here!
+def order_mocha():
+  while True:
+    res = input('Would you like to try our limited edition peppermint mocha \n[a] Sure! \n[b] Maybe next time! \n>')
 
-def order_latte():
-  res = input('And what kind of milk for your latte? \n [a] 2% milk \n [b] Non-fat milk \n [c] Soy milk \n')
-  if res == 'a':
-    return '2% milk latte'
-  elif res == 'b':
-    return 'Non-fat milk latte'
-  elif res == 'c':
-    return 'Soy milk latte'
-  else:
+    if res == 'a':
+      return 'peppermint mocha'
+    elif res == 'b':
+      return 'mocha'
+    
     print_message()
-    return order_latte()
+
+
+coffee_bot()
