92d0d759a9cfa2704691908497a824dc94d6abc3
Will <72624648+wgozzard@users.noreply.github.com>
2022-02-05 13:04:46 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,34 @@
+import sys
+def print_message():
+  print('I\'m sorry, I did not understand your selection. Please enter the corresponding letter for your response.')
+  
+def get_size():
+  res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
+  
+  stop_words = ['bye', 'stop']
+
+  if res == 'a':
+    return 'small'
+  elif res == 'b':
+    return 'medium'
+  elif res == 'c':
+    return 'large'
+  elif res in stop_words:
+    print("order cancelled")
+    sys.exit()
+  else:
+    print_message()
+    return get_size()
+
+def order_latte():
+  res = input('And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n> ')
+
+  if res == 'a':
+    return 'latte'
+  elif res == 'b':
+    return 'non-fat latte'
+  elif res == 'c':
+    return 'soy latte'
+  else:
+    print_message()
+    return order_latte()
